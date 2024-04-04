82bf3bd9ae438dcd8d5728259f46f52aafced85c
sgarnica1 <sgarnica1902@gmail.com>
2024-04-03 23:29:56 -0600

    Establish basic websocket

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,25 @@
-const express = require("express")
-const app = express()
-const port = 3000
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const port = 3000;
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket);
+  console.log(socket.id);
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "Websocket" });
 });
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
